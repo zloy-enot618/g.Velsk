6bd2f7e44fd2b1344aa6469f8029a26c88b2e230
#ReturnAN <o51393680@gmail.com>
2026-03-28 17:41:31 +0300

    Update twice.js

--- twice.js
+++ twice.js
@@ -1,11 +1,1 @@
-const clear = document.getElementById('change')
-const rename = document.getElementById('nedd')
-const butle = document.getElementById('tytle')
-const bodyStyle = document.getElementsByName('bodiee')
-
-clear.onclick = function() {
-    rename.textContent = 'Олег!';
-    clear.textContent = 'Олег!';
-    tytle.textContent = 'Олег!';
-    bodyStyle.style = 'background-color: black'
-}
+Олег!
